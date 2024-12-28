96e0fce422840587a61779c5ad319acca7d96187
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2024-12-29 02:24:27 +0330

    unstable state of snake latter.

--- z_tarahi_zaban/code/sl_2.pl
+++ z_tarahi_zaban/code/sl_2.pl
@@ -75,28 +75,27 @@ $mw->Button(
     -command => \&restart_game,
 )->pack(-side => 'bottom');
 
-# Manually define each step from 1 to 100
+# Manually define each step from 1 to 100 (bottom-left to top-right, left to right in rows)
 my @steps = (
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    20, 19, 18, 17, 16, 15, 14, 13, 12, 11,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    40, 39, 38, 37, 36, 35, 34, 33, 32, 31,
     41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-    51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    60, 59, 58, 57, 56, 55, 54, 53, 52, 51,
     61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-    71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+    80, 79, 78, 77, 76, 75, 74, 73, 72, 71,
     81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
-    91, 92, 93, 94, 95, 96, 97, 98, 99, 100
+    100, 99, 98, 97, 96, 95, 94, 93, 92, 91
 );
-
 
 # Draw the game board (10x10 grid)
 my $step = 0;
 for my $row (0 .. $BOARD_SIZE - 1) {
     for my $col (0 .. $BOARD_SIZE - 1) {
-        # Number the steps from 1 to 100 left-to-right
+        # Number the steps from 1 to 100 left-to-right, bottom-to-top
         my $x1 = $col * $SQUARE_SIZE;
-        my $y1 = $row * $SQUARE_SIZE;
+        my $y1 = ($BOARD_SIZE - 1 - $row) * $SQUARE_SIZE;  # Rows go up from bottom
         my $x2 = $x1 + $SQUARE_SIZE;
         my $y2 = $y1 + $SQUARE_SIZE;
 
