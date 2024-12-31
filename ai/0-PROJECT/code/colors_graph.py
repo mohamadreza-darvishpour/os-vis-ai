072a0e89bfb847040f7b50cb27b12a7c1ec6f2fc
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2024-12-31 18:35:38 +0330

    result message in ai ids output

--- ai/0-PROJECT/code/colors_graph.py
+++ ai/0-PROJECT/code/colors_graph.py
@@ -47,6 +47,84 @@ def make_out_file(p_col_dict:dict):
 
 
 
+import sys 
+import numpy as np
+
+
+class color_points_ids: 
+    def __init__(self , adjacency_mat:np.ndarray , num_colors:int ):
+        self.adjacency_mat = adjacency_mat 
+        self.num_colors = num_colors 
+        self.num_nodes =len(adjacency_mat)
+        self.colors = [-1] * self.num_nodes 
+        
+    def is_valid_color(self , node:int , color: int) -> bool : 
+        '''check if assigning color to node is valid'''
+        for neighbor_ind in range(self.num_nodes): 
+            if( self.adjacency_mat[node][neighbor_ind] ==1 and self.colors[neighbor_ind] == color):
+                return False 
+        return True 
+
+
+    def dfs(self , node:int , depth:int) -> bool:
+        '''first-depth-limited dfs for graph coloring '''
+        if node == self.num_nodes:
+            return True
+        if depth == 0 :
+            return False 
+        
+        for color in range(self.num_colors):
+            if self.is_valid_color(node , color):
+                self.colors[node] = color
+                if self.dfs(node+1 , depth-1):
+                    return True  
+                self.colors[node] = -1  #backtrack when not ending.
+        return False
+    
+    
+        
+
+    def ids_coloring(self) -> np.ndarray:
+        '''perform IDS for graph coloring.'''
+        for depth in range(1 , self.num_nodes+1):
+            if self.dfs(0 , depth):
+                return self.colors 
+        return False
+    
+    
+
+    def color_result(self) : 
+        color_list  = ['red' , 'green' , 'blue' , 
+                       'orange', 'white' , 'aqua' , 
+                       'yellow' , 'purple' , 'black',]
+        result_message ='' 
+        if self.ids_coloring() ==False : 
+            return 'error in \n\t\t1.adjacency matris\n\tor\n\t\t2.number of colors to use.\n'
+        for ind in range(  len(self.colors)):
+            #note to color_list[self.colors[ind]] it means get the color index number from colors in self 
+            result_message += f'{ind}={color_list[self.colors[ind]]},'
+        return result_message 
+
+
+
+
+
+
+adjacency_mat_ex = [
+    [0 ,1 ,1 ,0 ,1 ,0 ,1],
+    [1 ,0 ,1 ,0 ,0 ,0 ,0],
+    [1 ,1 ,0 ,0 ,1 ,0 ,1],
+    [0 ,0 ,0 ,0 ,0 ,1 ,0],
+    [1 ,0 ,1 ,0 ,0 ,0 ,0],
+    [0 ,0 ,0 ,1 ,0 ,0 ,0],
+    [1 ,0 ,1 ,0 ,0 ,0 ,0],
+]
+
+
+obj  = color_points_ids(adjacency_mat_ex , 3)
+obj.ids_coloring()
+result = obj.color_result() 
+print(result)
 
 
 
@@ -54,21 +132,8 @@ def make_out_file(p_col_dict:dict):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-print(lines_of_input_file(test_path))
-make_out_file(test_dict)
+# print(lines_of_input_file(test_path))
+# make_out_file(test_dict)
 
 
 
